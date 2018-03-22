0f723b9f7a3bc5c127023b9ad968c627b387fb34
spinmatt <37644978+spinmatt@users.noreply.github.com>
2018-03-21 23:06:44 -0600

    Add files via upload

--- Hangman/hangman.js
+++ Hangman/hangman.js
@@ -7,20 +7,7 @@ let score = 0;
 let livesRemaining = 10;
 //Set up the alphabet and key phrases. Initialize them to the document.
 let alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-let phrases = [
-	"I am the CrazyBobbles",
-	"Zef is Canadian",
-	"Bowbe loves to leave orbit",
-	"Harold is our Mascot",
-	"Katt Loves Books",
-	"Skilks has a new job",
-	"Crftsy is a son of a bitch",
-	"Moe and Liar are computer gods",
-	"Kye Makes Bread",
-	"Crittxr loves dogs",
-	"Corg is alright",
-	"Jules loves Carly"
-];
+
 //Get random phrase from phrases
 	function randomPhrase (phrases){
 	return phrases[Math.floor(Math.random()*phrases.length)];
---
+++ Hangman/phrases.js
@@ -0,0 +1,14 @@
+let phrases = [
+	"I am the CrazyBobbles",
+	"Zef is Canadian",
+	"Bowbe loves to leave orbit",
+	"Harold is our Mascot",
+	"Katt Loves Books",
+	"Skilks has a new job",
+	"Crftsy is a son of a bitch",
+	"Moe and Liar are computer gods",
+	"Kye Makes Bread",
+	"Crittxr loves dogs",
+	"Corg is alright",
+	"Jules loves Carly"
+];
