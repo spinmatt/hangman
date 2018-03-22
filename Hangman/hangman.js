0505bb6c24b33170c0f337f4b3c3d0d1482267ab
spinmatt <37644978+spinmatt@users.noreply.github.com>
2018-03-21 23:03:10 -0600

    Add files via upload

--- Hangman/hangman.js
+++ Hangman/hangman.js
@@ -1,21 +1,13 @@
-// +(function(){
-
-// })();
-
 //Start Game, collect player info & add it to H1 Element.
-	let playerName = prompt("What is your name, Raidbabe?");
-	let headerUpdate = document.getElementsByTagName("h1")[0].textContent + "<strong>" + playerName + "</strong>";
-	document.getElementsByTagName("h1")[0].innerHTML = headerUpdate;
-
-//Intializes the game upon instance.
-	function gameStart(){};
-			let score = 0;
-			let lives = 10;
-		
-	
+let playerName = prompt("What is your name, Raidbabe?");
+let headerUpdate = document.getElementsByTagName("h1")[0].textContent + "<strong>" + playerName + "</strong>";
+document.getElementsByTagName("h1")[0].innerHTML = headerUpdate;
+//Intializes the game.
+let score = 0;
+let livesRemaining = 10;
 //Set up the alphabet and key phrases. Initialize them to the document.
-	let alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-	let phrases = [
+let alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+let phrases = [
 	"I am the CrazyBobbles",
 	"Zef is Canadian",
 	"Bowbe loves to leave orbit",
@@ -28,8 +20,7 @@
 	"Crittxr loves dogs",
 	"Corg is alright",
 	"Jules loves Carly"
-	];
-
+];
 //Get random phrase from phrases
 	function randomPhrase (phrases){
 	return phrases[Math.floor(Math.random()*phrases.length)];
@@ -37,8 +28,6 @@
 	let secretArray = randomPhrase(phrases);
 	//To remove case issue
 	secretArray = secretArray.toUpperCase();
-
-
 //Display details for alphabet
 	//secret phrase with blanks 
 	let answerArray = [];
@@ -65,26 +54,46 @@
 //Get players guess
 	function playerGuess(){ 
 	let guess = prompt('Guess a Letter? or click cancel to stop playing');
-		guess = guess.toUpperCase();
 	let counter = 0;
+	let doubleGuess = true;
 	//let guess = document.getElementById("guess")[0].textContent;
+	
 
 	//Take the guess,check if it is valid, and run it against the secret word.
-	if (guess.length === null){
+	if (guess === null){
 		alert("This is not a correct guess!");
 		}else if (guess.length !== 1){
 		alert("This is not a correct guess!");
-		} else if (parseInt(guess)){
+		}else if (parseInt(guess)){
 		alert("This is not a correct guess!");
-		} else {
+		}else {
+			//Case match to avoid error
+			guess = guess.toUpperCase();
+		//Check if its a double guess and return nothing 
+		for (let k=0;k<alphabet.length;k++){
+
+			if (guess === alphabet[k]){
+				doubleGuess = false;
+			}
+		}	
+
+		//DoubleGuess end function
+		if (doubleGuess){
+			alert("You aready guessed this letter! Don't Guess it Again!");
+			return;
+		}
+
+
+		// Run the program to match guess to secret array	
 		for (let i=0;i<secretArray.length; i++){
+			
 			//check if guess is equal to string[i]
 			if (guess === secretArray[i]){
 				answerArray[i] = guess;
 				counter++;
 				guessedLetters--;
 				displayValues();
-				console.log(counter);
+				console.log(livesRemaining);
 
 
 			}
@@ -92,7 +101,7 @@
 		//Player loses a life if the letter was not in the secret word.
 			if (counter === 0){
 				alert("There were no " + guess + " in the puzzle; You lose a life!");
-				lives--;
+				livesRemaining--;
 
 		}
 
@@ -109,8 +118,6 @@
 				displayValues();
 				}
 			}
-
-			gameConditions();
 	 }
 	
 //Display text
@@ -120,11 +127,14 @@
 	document.getElementById("secretWord").innerHTML = answerArray;
 	document.getElementById("secretWord").innerHTML = answerArray.join(" ");
 	document.getElementById("score").innerHTML = "Total Score: " + score;
-	document.getElementById("lives").innerHTML = "Lives Remaining: " + lives;
+	document.getElementById("lives").innerHTML = "Lives Remaining: " + livesRemaining;
 	};
 //Initilize Button to starts playerGuess
 	let spinButton = document.getElementById("Spin");
-	spinButton.onclick = function() {playerGuess();};
+	spinButton.onclick = function() {
+		playerGuess();	
+		gameConditions();
+		};
 
 //create spin score wheel
 function spinScore(guess){
@@ -134,24 +144,60 @@ function spinScore(guess){
 
 //Solve Puzzle
 function solvePuzzle(){
+	
 	alert("Be careful babe, every wrong letter will cost you a life.");
-	let solveAnswer = prompt("What do you think the secret phrase is?");
-	//playerGuess(solveAnswer);
+	//Give user one chance if they enter too long of a string, otherwise end the game with zero points and lives.
+	let solveAnswer;
+	let j = 0;
+	do {
+	if (j===1){
+		alert("Your guess is not the same length than the answer.....Ill give you one more try!");
+		solveAnswer = prompt("What do you think the secret phrase is?").toUpperCase();
+	} else if (j>=2){
+		alert('Learn to follow the rules son! GAME OVER. You have zero mother fucking points.');
+		solveButton.style.visibility = "hidden";
+		spinButton.style.visibility = "hidden";
+		score = 0;
+		livesRemaining = 0;
+		displayValues();
+		return;
+	} else {
+	solveAnswer = prompt("What do you think the secret phrase is?").toUpperCase();
+	}
+	j++;
+	} while (solveAnswer.length !== secretArray.length);
+
+	if (solveAnswer === secretArray){
+		guessedLetters = 0;
+		score+=1000000;
+		
+	}
+	livesRemaining=0;
+	gameConditions();
 }
 //Solve Button
 let solveButton = document.getElementById("Solve");
-solveButton.onclick = function (){solvePuzzle()};
+solveButton.onclick = function (){
+	solvePuzzle()
+};
 
 //universal button selector(Listner for click then grab event and find id element, then run function based on selection)
 
 //If guessletters = 0 || lives = 0 game over
 function gameConditions(){
-	if (guessletters = 0){
-	alert("BABE YOU WON! You are a deadly babe that racked up over " + score + "points!");
-	} else if (lives = 0){
+	if (guessedLetters === 0){
+	alert("BABE YOU WON! You are a deadly babe that racked up over " + score + " points!");
+	solveButton.style.visibility = "hidden";
+	spinButton.style.visibility = "hidden";
+	} else if (livesRemaining === 0){
 	alert("You are not good at this game babe. Try again soon. You scored 0 points.")
-	} else
+	solveButton.style.visibility = "hidden";
+	spinButton.style.visibility = "hidden";
+	} else {
 	console.log("still alive babe.... Keep going!")
+	}
+	
+	displayValues();
 }
 
 
@@ -165,9 +211,3 @@ function gameConditions(){
 displayValues();
 
 
-
-// while (remainingLetters > 0) {
-
-
-// }
-//End Game Congratulations or Sad Face. Points.
